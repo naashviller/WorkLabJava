d70f755a028628e2d60c0be421a9fd73943b8378
Anastasia <naashviller@gmail.com>
2018-09-24 23:55:41 +0300

    first

---
+++ Callbacks/main.js
@@ -0,0 +1,15 @@
+function sum(arg1, arg2) {
+    return arg1 + arg2;
+}
+
+function mult(arg1, arg2) {
+    return arg1 * arg2;
+}
+
+function sub(arg1, arg2) {
+    return arg1 - arg2;
+}
+
+function calc(arg1, arg2, calcFunction) {
+    return calcFunction(arg1, arg2);
+}
